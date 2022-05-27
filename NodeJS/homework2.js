f0f843bb992bc19c0835aceb32ed954b142ce082
at-praew <67423313+at-praew@users.noreply.github.com>
2022-05-27 11:09:48 +0700

    edit

--- NodeJS/homework2.js
+++ NodeJS/homework2.js
@@ -1,14 +1,36 @@
-var arr1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var arr2 =[];
-console.log(arr1); 
+const char = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    let X
+    let y = " ";
 
-for(let i = 0 ; i < arr1.length;i++){
-    if (i%2===0 ){
-        arr2[i+1]=arr1[i]
-    }else{
-        arr2[i-1]=arr1[i]
+    word()
+    async function word(){
+        try{
+            for (i  = 0; i < char.length; i++){
+                if(i % 2 === 0){
+                    X = char[i];
+                }
+                i++
+
+                y += char[i]+" "+ x +" ";
+            } 
+            await console.log(y);
+        }catch{
+            console.error("Error");
+        }
     }
+
+
+
+// var arr2 =[];
+// console.log(arr1); 
+
+// for(let i = 0 ; i < arr1.length;i++){
+//     if (i%2===0 ){
+//         arr2[i+1]=arr1[i]
+//     }else{
+//         arr2[i-1]=arr1[i]
+//     }
     
 
-}
-console.log(arr2);
+// }
+// console.log(arr2);
